894d4d97df6a93a3fafd0a29e46b616233d6c7d9
Alex-DSJ <dengshijun233@gmail.com>
2021-11-02 00:10:32 -0400

    revert: restore customer files

---
+++ backend/models/customer.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../modules/sequelize');
+
+// const customer = () => {
+//     const Customer = sequelize.define('customers', {
+//         id: {
+//             type: DataTypes.STRING,
+//             primaryKey: true
+//         },
+//         firstName: {
+//             type: DataTypes.STRING,
+//         },
+//         lastName: {
+//             type: DataTypes.STRING,
+//         }
+//     }, {
+//         timestamps: false,
+//         freezeTableName: true,
+//     });
+//
+//     Customer.sync().then(() => {
+//         console.log('table \"customers\" created');
+//     });
+//
+//     return Customer;
+// }
+
+const Customer = sequelize.define(
+  'customers',
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
+
+module.exports = Customer;
