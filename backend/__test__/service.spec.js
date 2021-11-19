d20dbb58a14e9bea585a3f04645f1c7818c7835f
yuxiang-zhang <yxz.get@gmail.com>
2021-11-19 18:11:17 -0500

    test: add service model test statements

--- backend/__test__/service.spec.js
+++ backend/__test__/service.spec.js
@@ -2,3 +2,52 @@ const { sequelize } = require('../modules/sequelize');
 const { DataTypes } = require('sequelize');
 const { Service } = require('../models/service');
 
+jest.mock('../modules/sequelize');
+
+describe('Service Model', () => {
+  it('should setup the service table', async () => {
+    expect(sequelize.define).toBeCalledWith(
+      'services',
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    treatment_type: {
+      type: DataTypes.STRING,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+    },
+    price: {
+      type: DataTypes.STRING,
+    },
+    barcode: {
+      type: DataTypes.STRING,
+    },
+    sms_description: {
+      type: DataTypes.STRING,
+    },
+    blocked: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      default: false,
+    },
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+  }
+    );
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+});
