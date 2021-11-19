7fcec728f6afadc7eb72299eb11d05e5d4d5d2e0
Alex-DSJ <dengshijun233@gmail.com>
2021-11-19 01:59:33 -0500

    feat: config the backend with typescript

---
+++ backend/src/modules/sequelize.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from 'sequelize';
+import config from '../config';
+
+let sequelize: Sequelize = new Sequelize({
+  host: config.database.mysql.host,
+  port: parseInt(config.database.mysql.port),
+  database: config.database.mysql.database,
+  username: config.database.mysql.username,
+  password: config.database.mysql.password,
+  dialect: 'mysql',
+  logging: false,
+  define: {
+    freezeTableName: true,
+  },
+});
+
+export default sequelize;
---
+++ backend/src/server.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import sequelize from './modules/sequelize';
+import config from './config';
+
+const startServer = async () => {
+  const app = express();
+
+  app.use(express.json());
+  // app.use('/api/v1', router);
+
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log(
+        'Sequelize successfully connected to MySQL database on port:  ' +
+          config.database.mysql.port
+      );
+    })
+    .catch((error) => {
+      console.error('ERROR - connection failed: ', error);
+    });
+
+  sequelize
+    .sync({
+      alter: true,
+    })
+    .then(() => {
+      console.log('ALl database tables have been synchronized');
+    })
+    .catch((error) => {
+      console.error('ERROR - database table synchronization failed: ', error);
+    });
+
+  const PORT = config.port || 5000;
+
+  app.listen(PORT, () => {
+    console.log('server starts on port: ', PORT);
+  });
+};
+
+startServer();
