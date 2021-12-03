3b4d091e092b69d3d852138af7ad29e7161fcc87
yuxiang-zhang <yxz.get@gmail.com>
2021-12-03 07:24:00 -0500

    feat: adjust side calendar style according to design

--- frontend/components/Layout/calendar.js
+++ frontend/components/Layout/calendar.js
@@ -1,13 +1,74 @@
 import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import StaticDatePicker from '@mui/lab/StaticDatePicker';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat, sans-serif',
+    color: 'white',
+    fontSize: 16,
+  },
+  components: {
+    MuiCalendarPicker: {
+      styleOverrides: {
+        root: {
+          color: 'white',
+          background: '#888888',
+        },
+      },
+    },
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          color: 'white',
+          ':hover': {
+            backgroundColor: 'rgba(255,255,255,0.4)',
+          }
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          fontWeight: 'bold',
+          color: 'rgba(255,255,255,1)',
+          caption: {
+            color: 'rgba(255,255,255,1)'
+          }
+        },
+      },
+    },
+    MuiPickersDay: {
+      styleOverrides: {
+        root: {
+          color: 'white',
+          background: '#888888',
+          fontSize: 14,
+          "&.Mui-selected": {
+            fontWeight: 'bold',
+            color: 'black',
+            background: '#E8E8E8',
+            ':hover': {
+              backgroundColor: 'rgba(255,255,255,0.4)',
+            }
+          },
+          ':hover': {
+            color: 'black',
+            backgroundColor: 'rgba(255,255,255,0.4)',
+          }
+        },
+      },
+    },
+  },
+});
+
 const Calendar = () => {
   const [date, setDate] = React.useState(new Date());
 
-  return (<LocalizationProvider dateAdapter={AdapterDateFns}>
+  return (<ThemeProvider theme={theme}><LocalizationProvider dateAdapter={AdapterDateFns}>
     <StaticDatePicker
       displayStaticWrapperAs="desktop"
       // openTo="year"
@@ -17,7 +78,7 @@ const Calendar = () => {
       }}
       renderInput={(params) => <TextField {...params} />}
     />
-  </LocalizationProvider>
+  </LocalizationProvider></ThemeProvider>
   );
 };
 export default Calendar;
