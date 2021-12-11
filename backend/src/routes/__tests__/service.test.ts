d3f61e44afb0345cb3775c645697cd7d797b423b
yuxiang-zhang <yxz.get@gmail.com>
2021-12-10 22:10:06 -0500

    refactor: move statements to caller to reduce dependency

--- backend/src/routes/__tests__/service.test.ts
+++ backend/src/routes/__tests__/service.test.ts
@@ -1,35 +1,31 @@
-import app from '../../server';
-import request from 'supertest';
+import express from 'express';
+import cors from 'cors';
+import request, { SuperTest } from 'supertest';
+import serviceRouter from '../service';
 
 jest.useFakeTimers('legacy');
-
-jest.mock('sequelize', () => ({
-  ...jest.requireActual('sequelize'),
-  authenticate: jest.fn(),
-  Model: jest.fn(),
-}));
-
-jest.mock('../../modules/sequelize', () => ({
-  Sequelize: jest.fn(),
-  model: jest.fn(),
-  authenticate: jest.fn(() => {
-  return Promise.resolve('value');
-}),
-}));
 
 jest.mock('../../models/service', () => ({
   init: jest.fn(),
 }));
 
-jest.mock('../../models/syncTables');
-
 jest.mock('../../services/service-service', () => ({
   getAllValidItems: () => Promise.resolve({}),
 }));
 
+let server: SuperTest<request.Test>;
+
+beforeAll(() => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use('/services', serviceRouter);
+  app.listen(5000);
+  server = request(app);
+});
+
 describe('Service Routes', () => {
   it('should respond to /services', async () => {
-    const server = request(app);
-    await server.get('/api/v1/services').expect(200);
+    await server.get('/services').expect(200);
   });
 });
