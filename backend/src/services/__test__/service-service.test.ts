3cb6e5ee4ea888d1f4c6a5a3c399a92f386479ed
yuxiang-zhang <yxz.get@gmail.com>
2021-12-11 23:50:19 -0500

    test: add test for getAllValidItems service in Service module

---
+++ backend/src/services/__test__/service-service.test.ts
@@ -0,0 +1,26 @@
+import serviceService from '../service-service';
+
+jest.mock('sequelize', () => ({
+  ...jest.requireActual('sequelize'),
+  authenticate: jest.fn(),
+  Model: jest.fn(),
+}));
+
+jest.mock('../../modules/sequelize', () => ({
+  Sequelize: jest.fn(),
+  model: jest.fn(() => ({
+      findAll: jest.fn(() => Promise.resolve([])),
+  })),
+  authenticate: jest.fn(() => {
+    return Promise.resolve('value');
+  }),
+}));
+
+const getAllValidItems = jest.spyOn(serviceService, 'getAllValidItems');
+
+describe('Services for Service Module', () => {
+  it.only('should get all valid items as json', async () => {
+    serviceService.getAllValidItems();
+    expect(getAllValidItems).toReturnWith(Promise.resolve({}));
+  });
+});
