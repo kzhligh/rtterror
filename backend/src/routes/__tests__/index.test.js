477e164327283d183fb83518b7a58292f78d88e2
yuxiang-zhang <yxz.get@gmail.com>
2021-12-20 00:51:50 -0500

    test: update tests with combo router from PR #20

--- backend/src/routes/__tests__/index.test.js
+++ backend/src/routes/__tests__/index.test.js
@@ -1,4 +1,5 @@
 import serviceRouter from '../service';
+import comboRouter from '../combo';
 
 const useSpy = jest.fn();
 
@@ -9,10 +10,12 @@ jest.doMock('express', () => ({
 }));
 
 jest.mock('../service', () => jest.fn());
+jest.mock('../combo', () => jest.fn());
 
 describe('should test server configuration', () => {
   it('should use router', () => {
     require('../index.ts');
     expect(useSpy).toHaveBeenCalledWith('/services', serviceRouter);
+    expect(useSpy).toHaveBeenCalledWith('/combos', comboRouter);
   });
 });
