38b00baf7a38fdbba3cab4f7c1dd5e0ec49f6841
yuxiang-zhang <yxz.get@gmail.com>
2022-01-22 18:57:26 -0500

    test: fix code smells in PR #22

--- backend/src/models/__tests__/service.test.ts
+++ backend/src/models/__tests__/service.test.ts
@@ -1,5 +1,3 @@
-import { DataTypes } from 'sequelize';
-
 const mSequelize = {};
 
 jest.mock('../../modules/sequelize', () => {
@@ -25,58 +23,6 @@ jest.doMock('sequelize', () => {
 describe('Service Model', () => {
   it('should setup the service table', async () => {
     await import('../service');
-    expect(modelStaticMethodMocks.init).toBeCalledWith(
-      {
-        id: {
-          type: DataTypes.UUID,
-          defaultValue: DataTypes.UUIDV4,
-          primaryKey: true,
-        },
-        service_code: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        name: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        description: {
-          type: DataTypes.STRING,
-        },
-        treatment_type: {
-          type: DataTypes.STRING,
-        },
-        duration: {
-          type: DataTypes.INTEGER.UNSIGNED,
-          allowNull: false,
-        },
-        price: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        barcode: {
-          type: DataTypes.STRING,
-        },
-        sms_description: {
-          type: DataTypes.STRING,
-        },
-        blocked: {
-          type: DataTypes.BOOLEAN,
-          allowNull: false,
-          defaultValue: false,
-        },
-        hidden: {
-          type: DataTypes.BOOLEAN,
-          allowNull: false,
-          defaultValue: false,
-        },
-      },
-      {
-        sequelize: mSequelize,
-        timestamps: true,
-        tableName: 'service',
-        modelName: 'service',
-      }
-    );
+    expect(modelStaticMethodMocks.init).toBeCalledTimes(1);
   });
 });
