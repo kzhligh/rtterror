349c2d287ad2346ccaa5d1540498ccb315c7d342
Alex-DSJ <dengshijun233@gmail.com>
2022-01-29 23:47:01 -0500

    refactor: extra function and reduce duplicate

--- backend/src/routes/employee.ts
+++ backend/src/routes/employee.ts
@@ -1,21 +1,8 @@
 import express from "express";
 import employeeService from '../services/employee-service';
+import { sendBackErrorMessage } from "src/routes/errorProcess";
 
 const router = express.Router();
-
-const sendBackErrorMessage = (
-    req: express.Request,
-    res: express.Response,
-    error: Error
-) => {
-    console.error(`ERROR - (${req.method})/services${req.path}/error: ${error}`);
-    res.status(400).send(error.message);
-};
-
-router.use(function (req, res, next) {
-    console.log('%s %s %s', req.method, req.url, req.path);
-    next();
-});
 
 router.post('/', (req, res) => {
     const { body } = req;
