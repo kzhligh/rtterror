262d07b77be3f7ac4f6ca6a95e6732c6dcc21bc3
qjimmy <quang-jimmy@hotmail.com>
2022-02-20 17:42:37 -0500

    fix: lighter containers by chown to node and nextjs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  experimental: {
+    outputStandalone: true,
+  },
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_CLIENT_HOST: process.env.NEXT_PUBLIC_CLIENT_HOST,
+  },
 };
