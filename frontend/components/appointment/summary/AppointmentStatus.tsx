c97442fbfc059402b42cd7bd1f753cfaca7696de
yuxiang-zhang <yxz.get@gmail.com>
2022-03-26 21:19:31 -0400

    feat: add history view for appm status

--- frontend/components/appointment/summary/AppointmentStatus.tsx
+++ frontend/components/appointment/summary/AppointmentStatus.tsx
@@ -1,4 +1,14 @@
-import { InputLabel } from '@mui/material';
+import { useState } from 'react';
+import {
+  InputLabel,
+  Tooltip,
+  Typography,
+  Menu,
+  MenuItem,
+  IconButton,
+  Grid,
+} from '@mui/material';
+import { History, Edit } from '@mui/icons-material';
 import {
   Timeline,
   TimelineItem,
@@ -8,24 +18,78 @@ import {
   TimelineConnector,
   TimelineContent,
 } from '@mui/lab';
+import moment from 'moment';
 
-export const AppointmentStatus = ({ statuses, setForm }) => (
-  <>
-    <InputLabel style={{ marginTop: '5%' }}>Status</InputLabel>
-    <Timeline position='left'>
-      {console.log(statuses)}{' '}
-      {statuses.map(() => (
-        <TimelineItem>
-          <TimelineOppositeContent color='text.secondary'>
-            09:30 am
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineConnector />
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>Eat</TimelineContent>
-        </TimelineItem>
-      ))}
-    </Timeline>
-  </>
-);
+interface IStatus {
+  name: string;
+  by: string;
+  at: Date;
+}
+
+export const AppointmentStatus = ({ statuses, setForm }) => {
+  const [historyAnchor, setHistoryAnchor] = useState(null);
+  const openHistory = Boolean(historyAnchor);
+  const [openUpdateStatus, setOpenUpdateStatus] = useState(false);
+  const handleClickHistory = (event) => {
+    setHistoryAnchor(event.currentTarget);
+  };
+  const handleCloseHistory = () => {
+    setHistoryAnchor(null);
+  };
+
+  return (
+    <>
+      <Grid
+        container
+        style={{ marginTop: '5%' }}
+        justifyContent='space-between'
+      >
+        <InputLabel>
+          Status
+          <IconButton onClick={handleClickHistory} size='small'>
+            <History />
+          </IconButton>
+        </InputLabel>
+        <Typography variant='button' color='InfoText'>
+          {statuses.length && statuses[statuses.length - 1].name}
+          <IconButton onClick={() => setOpenUpdateStatus(true)} size='small'>
+            <Edit />
+          </IconButton>
+        </Typography>
+        <div></div>
+      </Grid>
+      <Menu
+        anchorEl={historyAnchor}
+        id='account-menu'
+        open={openHistory}
+        onClose={handleCloseHistory}
+      >
+        <Timeline position='left'>
+          {statuses.map((status: IStatus) => (
+            <MenuItem
+              disableRipple
+              style={{ cursor: 'default', backgroundColor: 'transparent' }}
+            >
+              <Tooltip title={`by ${status.by}`} enterDelay={0}>
+                <TimelineItem>
+                  <TimelineOppositeContent color='text.secondary'>
+                    <Typography variant='caption'>
+                      {moment(status.at).calendar()}
+                    </Typography>
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineConnector />
+                    <TimelineDot />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant='button'>{status.name}</Typography>
+                  </TimelineContent>
+                </TimelineItem>
+              </Tooltip>
+            </MenuItem>
+          ))}
+        </Timeline>
+      </Menu>
+    </>
+  );
+};
